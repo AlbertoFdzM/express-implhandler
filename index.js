a7928d14aab9ec7dead5839b052c03758481dcfb
Alberto Fernandez <albertofdzm@gmail.com>
2016-01-26 12:27:20 +0100

    express-implhandler now accepts a callback arg

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
-var express = require('express');
 var listEndpoints = require('express-list-endpoints');
 
 /**
  * Puts middleware to set 501 error for not used methods
  */
-var implHandler = function(router) {
+var implHandler = function(router, callback) {
   var endpoints = [];
+  var defaultCb = function(req, res, next) {
+    var err = new Error('Not implemented');
+
+    err.status = 501;
+    return next(err);
+  };
 
   router = router || {};
+  callback = callback || defaultCb;
 
-  if (router.tack) {
+  if (router.stack) {
     endpoints = listEndpoints(router.stack);
   } else if (router._router) {
     endpoints = listEndpoints(router._router.stack);
   }
 
   endpoints.map(function(endpoint) {
-    router.all(endpoint.path, function(req, res, next) {
-      var err = new Error('Not implemented');
-
-      err.status = 501;
-      next(err);
-    });
+    router.all(endpoint.path, callback);
   });
 };
 
