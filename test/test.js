3aa710c895c7becc2f4b67e1f5c4ec882ea2ee0b
Alberto Fernandez <albertofdzm@gmail.com>
2016-02-09 08:32:09 +0100

    Improved test with promises

--- test/test.js
+++ test/test.js
@@ -5,6 +5,7 @@ var express = require('express');
 var implHandler = require('../index');
 var expect = chai.expect;
 var should = chai.should();
+var Q = global.Promise;
 
 var app = express();
 var router = express.Router();
@@ -52,98 +53,72 @@ describe('express-implhandler', function() {
     describe('for the app', function() {
       describe('on defined endpoints', function() {
         it('should not change response on defined methods', function(done) {
-          agent.get('/')
-            .end(function(err, res) {
-              expect(err).to.be.null;
+          Q.all([
+            agent.get('/'),
+            agent.put('/app-test')
+          ]).then(function (values) {
+            values.forEach(function(res) {
               expect(res).to.has.status(200);
             });
-          agent.put('/app-test')
-            .end(function(err, res) {
-              expect(err).to.be.null;
-              expect(res).to.has.status(200);
-              done();
-            });
+            done();
+          });
         });
 
         it('should set an error code 501 on undefined methods', function(done) {
-          agent.post('/')
-            .end(function(err, res) {
-              expect(res.error).to.be.ok;
+          Q.all([
+            agent.post('/'),
+            agent.delete('/app-test')
+          ]).then(function(values) {
+            values.forEach(function(res) {
               expect(res).to.has.status(501);
             });
-          agent.delete('/app-test')
-            .end(function(err, res) {
-              expect(res.error).to.be.ok;
-              expect(res).to.has.status(501);
-              done();
-            });
+            done();
+          });
         });
       });
 
       describe('on undefined endpoints', function() {
         it('should not set any handler', function(done) {
-          agent.get('/some-endpoint')
-            .end(function(err, res) {
-              expect(err).to.be.null;
+          Q.all([
+            agent.get('/some-endpoint'),
+            agent.post('/another-endpoint')
+          ]).then(function(values) {
+            values.forEach(function(res) {
               expect(res).to.has.status(404);
             });
-          agent.post('/another-endpoint')
-            .end(function(err, res) {
-              expect(err).to.be.null;
-              expect(res).to.has.status(404);
-              done();
-            });
+            done();
+          });
         });
       });
     });
 
     describe('for the attached routers methods', function() {
       it('should not change the response on current methods', function(done) {
-        agent.get('/router/')
-          .end(function(err, res) {
-            expect(err).to.be.null;
+        Q.all([
+          agent.get('/router/'),
+          agent.post('/router/router-test'),
+          agent.copy('/router/router-test'),
+          agent.delete('/router/router-test')
+        ]).then(function(values) {
+          values.forEach(function(res) {
             expect(res).to.has.status(200);
           });
-        agent.post('/router/router-test')
-          .end(function(err, res) {
-            expect(err).to.be.null;
-            expect(res).to.has.status(200);
-          });
-        agent.copy('/router/router-test')
-          .end(function(err, res) {
-            expect(err).to.be.null;
-            expect(res).to.has.status(200);
-          });
-        agent.delete('/router/router-test')
-          .end(function(err, res) {
-            expect(err).to.be.null;
-            expect(res).to.has.status(200);
-            done();
-          });
+          done();
+        });
       });
 
       it('should not set handlers for undefined endopints', function(done) {
-        agent.get('/router/some-endpoint')
-          .end(function(err, res) {
-            expect(err).to.be.null;
+        Q.all([
+          agent.get('/router/some-endpoint'),
+          agent.put('/router/some-endpoint'),
+          agent.post('/router/another-endpoint'),
+          agent.delete('/router/another-endpoint')
+        ]).then(function(values) {
+          values.forEach(function(res) {
             expect(res).to.has.status(404);
           });
-        agent.put('/router/some-endpoint')
-          .end(function(err, res) {
-            expect(err).to.be.null;
-            expect(res).to.has.status(404);
-          });
-        agent.post('/router/another-endpoint')
-          .end(function(err, res) {
-            expect(err).to.be.null;
-            expect(res).to.has.status(404);
-          });
-        agent.delete('/router/another-endpoint')
-          .end(function(err, res) {
-            expect(err).to.be.null;
-            expect(res).to.has.status(404);
-            done();
-          });
+          done();
+        });
       });
     });
   });
