b098001ef25b8f53a4530fa9be78a6409fd91925
Alberto Fernandez <albertofdzm@gmail.com>
2016-04-21 13:32:43 +0200

    Fix lint errors and improved tests Closes #3

--- test/test.js
+++ test/test.js
@@ -4,51 +4,66 @@ chai.use(require('chai-http'));
 var express = require('express');
 var implHandler = require('../index');
 var expect = chai.expect;
-var should = chai.should();
 var Q = global.Promise;
 
 var app = express();
 var router = express.Router();
 
+var multiCatch = function(promises, errHandler, cb, index) {
+  index = index || 0;
+  promises[index].catch(function(err) {
+    errHandler(err);
+    index++;
+    if (index < promises.length) {
+      multiCatch(promises, errHandler, cb, index);
+    } else {
+      cb();
+    }
+  });
+};
+
+var checkResponses = function(code, values, cb) {
+  values.forEach(function(res) {
+    expect(res).to.has.status(code);
+  });
+  cb();
+};
+
+var end = function(req, res) {
+  return res.end();
+};
+
 var errorHandler = function(err, req, res, next) {
-  if (err) {return res.status(err.status || 500).json(err);}
+  if (err) {
+    return res.status(err.status || 500).json(err);
+  }
 
   return next();
 };
 
 router.route('/')
-  .get(function(req, res) {
-    return res.end();
-  });
+  .get(end);
 
 router.route('/router-test')
-  .post(function(req, res) {
-    return res.end();
-  })
-  .copy(function(req, res) {
-    return res.end();
-  })
-  .delete(function(req, res) {
-    return res.end();
-  });
+  .post(end)
+  .copy(end)
+  .delete(end);
 
 app.route('/')
-  .get(function(req, res) {
-    return res.end();
-  });
+  .get(end);
 
 app.route('/app-test')
-  .put(function(req, res) {
-    return res.end();
-  });
+  .put(end);
 
 app.use('/router', router);
 
 describe('express-implhandler', function() {
   describe('when called over an app', function() {
+    var agent;
+
     implHandler(app);
     app.use(errorHandler);
-    var agent = chai.request.agent(app);
+    agent = chai.request.agent(app);
 
     describe('for the app', function() {
       describe('on defined endpoints', function() {
@@ -57,37 +72,28 @@ describe('express-implhandler', function() {
             agent.get('/'),
             agent.put('/app-test')
           ]).then(function (values) {
-            values.forEach(function(res) {
-              expect(res).to.has.status(200);
-            });
-            done();
+            checkResponses(200, values, done);
           });
         });
 
         it('should set an error code 501 on undefined methods', function(done) {
-          Q.all([
+          multiCatch([
             agent.post('/'),
             agent.delete('/app-test')
-          ]).then(function(values) {
-            values.forEach(function(res) {
-              expect(res).to.has.status(501);
-            });
-            done();
-          });
+          ], function(err) {
+            expect(err).to.has.status(501);
+          }, done);
         });
       });
 
       describe('on undefined endpoints', function() {
         it('should not set any handler', function(done) {
-          Q.all([
+          multiCatch([
             agent.get('/some-endpoint'),
             agent.post('/another-endpoint')
-          ]).then(function(values) {
-            values.forEach(function(res) {
-              expect(res).to.has.status(404);
-            });
-            done();
-          });
+          ], function(err) {
+            expect(err).to.has.status(404);
+          }, done);
         });
       });
     });
@@ -100,25 +106,19 @@ describe('express-implhandler', function() {
           agent.copy('/router/router-test'),
           agent.delete('/router/router-test')
         ]).then(function(values) {
-          values.forEach(function(res) {
-            expect(res).to.has.status(200);
-          });
-          done();
+          checkResponses(200, values, done);
         });
       });
 
       it('should not set handlers for undefined endopints', function(done) {
-        Q.all([
+        multiCatch([
           agent.get('/router/some-endpoint'),
           agent.put('/router/some-endpoint'),
           agent.post('/router/another-endpoint'),
           agent.delete('/router/another-endpoint')
-        ]).then(function(values) {
-          values.forEach(function(res) {
-            expect(res).to.has.status(404);
-          });
-          done();
-        });
+        ], function(err) {
+          expect(err).to.has.status(404);
+        }, done);
       });
     });
   });
