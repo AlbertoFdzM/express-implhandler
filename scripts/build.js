e6f52672757f818243b2e4b61cce674c2b94395b
Alberto Fernandez <albertofdzm@gmail.com>
2016-11-05 17:02:09 +0100

    Ignore eslint console statements in build script and little refactor

--- scripts/build.js
+++ scripts/build.js
@@ -5,14 +5,16 @@ var pkgInfo = require('../package.json');
 var changelog = path.join(__dirname, '../CHANGELOG.md');
 
 fs.readFile(changelog, 'utf8', function(err, data) {
+  // eslint-disable-next-line no-console
   if (err) return console.err(err);
 
   var version = 'v' + pkgInfo.version;
   var date = new Date().toISOString().replace(/T.*/, '');
 
-  var result = data.replace(/\[UNRELEASED\]/g,  version + ' ' + date);
+  var result = data.replace(/\[UNRELEASED\]/g,  version + ' - ' + date);
 
   fs.writeFile(changelog, result, 'utf8', function (err) {
-     if (err) return console.err(err);
+    // eslint-disable-next-line no-console
+    if (err) return console.err(err);
   });
 });
