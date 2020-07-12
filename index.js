b2c1f5f7b89273073caa11f520ac25539a86d7b4
Alberto Fernandez <albertofdzm@gmail.com>
2020-07-12 18:32:49 +0200

    reafctor(general): deprecate use of var in favor of const and let

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
-var listEndpoints = require('express-list-endpoints')
+const listEndpoints = require('express-list-endpoints')
 
 /**
  * Default callback
  */
-var defaultCb = function (req, res, next) {
-  var err = new Error('Not implemented')
+const defaultCb = function (req, res, next) {
+  const err = new Error('Not implemented')
 
   err.status = 501
+
   return next(err)
 }
 
 /**
  * Puts middleware to set 501 error for unused methods
  */
-var implHandler = function (app, callback) {
-  var endpoints = listEndpoints(app)
+const implHandler = function (app, callback) {
+  const endpoints = listEndpoints(app)
   callback = callback || defaultCb
 
   endpoints.map(function (endpoint) {
